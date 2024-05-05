f8d5b4ce30a9dfb2e800781e12a0fffe38cdc894
AbdulMoeez825 <moeeznaseer512@gmail.com>
2024-05-05 19:49:39 +0500

    new commit

---
+++ Assignments/Assignment 11/index.js
@@ -0,0 +1,52 @@
+// Q # 1
+let a = 2;
+let b = ++a * 2; 
+console.log(" Q # 1= " +b)
+
+
+// Q # 2
+let x = 5;
+let y = x-- + 2;
+console.log(" Q # 2= " +y)
+
+// Q # 3
+let x1 = 3;
+let y1 = ++x1 + x1++ + ++x1;
+console.log(" Q # 3= " +y1)
+
+// Q # 4
+
+let m = 2;
+let n = ++m * m++ * --m;
+console.log(" Q # 4= " +n)
+
+// Q # 5
+
+let a1 = 3;
+let b1 = 5;
+let result = ++a1 + b1-- - a1++ + --b1;
+console.log(" Q # 5= " +result)
+
+// Q # 6
+
+let m1 = 2;
+let n1 = 4;
+let p1 = m1++ + ++n1 - --m1 + n1--;
+console.log(" Q # 6 " + " p1=" +p1, " m1=" + m1, " n1=" + n1,)
+
+// Q # 7
+
+ let a2 = 5;
+ let b2 = 3;
+ let c2 = 2;
+ let d2 = 7;
+ let result2 = ++a2 * (b2-- + c2) / --d2;
+console.log(" Q # 7= " + result2)
+
+// Q # 8
+
+let m3 = 2;
+let n3 = 3;
+let o3 = 4;
+let result3 = m3++ * (--n3 + m3) / (o3-- - n3)
+console.log(" Q # 8 " + " result3=" + result3, " m3=" + m3, " n3=" + n3, " O3=" + o3)
---
+++ Practice/practice java/new.js
@@ -0,0 +1,10 @@
+// var num = 10;
+// var num1 = 20;
+// var total = num + num1;
+
+// console.log(total)
+// console.log(num1)
+var num = 10;
+num = num + 3;
+num++
+console.log(num)
